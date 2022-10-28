e2d6b0093a44e022921604282a53b44d712953aa
Michael Thompson <thompsonmichaelj2020@gmail.com>
2022-10-28 14:18:54 -0400

    Mini-lab complete

--- cypress/e2e/App.cy.js
+++ cypress/e2e/App.cy.js
@@ -1,5 +1,8 @@
 describe('empty spec', () => {
   it('passes', () => {
     cy.visit('http://localhost:3000/')
+    cy.get('p')
+    cy.get('.App-link').contains('Learn')
+    cy.get('button')
   })
 })
